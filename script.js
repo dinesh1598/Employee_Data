161f564e6aff17f39682fcb58a739d2077614e3e
dineshThanigaivel <101261835+dinesh1598@users.noreply.github.com>
2022-03-24 15:13:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+fetch(`https://run.mocky.io/v3/010e898c-a05c-4a0a-b947-2a65b5a267c5`).then(
+    res => {
+      res.json().then(
+        data => {
+          console.log(data);
+          if (data.length > 0) {
+  
+            var temp = "";
+            data.forEach((itemData) => {
+              temp += "<tr>";
+              temp += "<td>" + itemData.first_name+ "</td>";
+              temp += "<td>" + itemData.last_name+ "</td>";
+              temp += "<td>" + itemData.username + "</td>";
+              temp += "<td>" + itemData.employment.title+ "</td>";
+              temp += "<td>" + itemData.address.country+"</td></tr>";
+              
+            });
+            document.getElementById('data').innerHTML = temp;
+          }
+        }
+      )
+    }
+  );
